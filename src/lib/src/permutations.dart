a6e11ec169126cc761a6e9325e9377492a99312b
Rody Davis <rody.davis.jr@gmail.com>
2020-04-16 17:13:04 -0700

    permutations

---
+++ src/lib/src/permutations.dart
@@ -0,0 +1,29 @@
+// main(List<String> args) {
+//   final _result = permutations([1, 2, 3]);
+//   print('permutations -> $_result');
+// }
+
+List<List<int>> permutations(List<int> array) {
+  final List<List<int>> _results = [];
+  swapForPermutations(0, array, _results);
+  return _results;
+}
+
+void swapForPermutations(
+    int index, List<int> array, List<List<int>> permutations) {
+  if (index == array.length - 1) {
+    permutations.add(List.from(array));
+  } else {
+    for (var j = index; j < array.length; j++) {
+      swap(array, index, j);
+      swapForPermutations(index + 1, array, permutations);
+      swap(array, index, j);
+    }
+  }
+}
+
+void swap(List<int> array, int a, int b) {
+  final item = array[a];
+  array[a] = array[b];
+  array[b] = item;
+}
